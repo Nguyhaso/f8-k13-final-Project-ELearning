22b21c094dffdc3c093422d10e20205c249d7c58
nguyhaso <94haison@gmail.com>
2025-06-09 00:07:00 +0700

    loginpage

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// export default defineConfig({
-//   plugins: [react(), tsconfigPaths()],
-//   server: {
-//     host: true,
-//     port: 3000
-//   }
-// });
-
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/f8-k13-final-Project-ELearning",
-})
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 3000
+  }
+});
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: import.meta.env.VITE_BASE_PATH || "/f8-k13-final-Project-ELearning",
+// })s
